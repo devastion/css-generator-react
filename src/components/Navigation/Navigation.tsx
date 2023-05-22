2c9a65252c87ceeb74e6a217c12e28bb836a88f6
Dimitar Banev <banev@enetpulse.com>
2023-05-22 19:41:49 +0300

    chore: setup gh pages deploy

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -14,16 +14,31 @@ const navLinks: { [key: string]: string } = {
   Transform: "/transform",
 };
 
+const navLinksProd: { [key: string]: string } = {
+  "Border Radius": "/css-generator-react/border-radius",
+  "Box Shadow": "/css-generator-react/box-shadow",
+  "Text Shadow": "/css-generator-react/text-shadow",
+  RGBA: "/css-generator-react/rgba",
+  "Font Face": "/css-generator-react/font-face",
+  "Multiple Columns": "/css-generator-react/multiple-columns",
+  "Box Resize": "/css-generator-react/box-resize",
+  "Box Sizing": "/css-generator-react/box-sizing",
+  Outline: "/css-generator-react/outline",
+  Transition: "/css-generator-react/transition",
+  Transform: "/css-generator-react/transform",
+};
+
 export const Navigation = () => {
+  const links = import.meta.env.MODE === "production" ? navLinksProd : navLinks;
   return (
     <div>
-      <NavLink to="/">
+      <NavLink to={import.meta.env.MODE === "production" ? "/css-generator-react/" : "/"}>
         <h1 className="text py-2 text-center text-xl font-bold text-black">CSS Generator</h1>
       </NavLink>
       <ul className="scrollbar-hide flex gap-5 overflow-scroll whitespace-nowrap text-black md:flex-wrap">
-        {Object.keys(navLinks).map((key, index) => {
+        {Object.keys(links).map((key, index) => {
           return (
-            <NavLink key={index} to={navLinks[key]} className={({ isActive }) => (isActive ? "text-blue" : "")}>
+            <NavLink key={index} to={links[key]} className={({ isActive }) => (isActive ? "text-blue" : "")}>
               <li>{key}</li>
             </NavLink>
           );
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,117 @@ import { Navigation } from "components/Navigation";
 import { ErrorPage } from "components/ErrorPage";
 import { BorderRadius, BoxShadow, TextShadow, RGBA, FontFace, MultipleColumns, BoxResize, BoxSizing, Outline, Transition, Transform } from "./pages/";
 
+const routerProd = createBrowserRouter([
+  {
+    path: "/css-generator-react/",
+    element: (
+      <Core>
+        <Navigation />
+      </Core>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/css-generator-react/border-radius",
+    element: (
+      <Core>
+        <Navigation />
+        <BorderRadius />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/box-shadow",
+    element: (
+      <Core>
+        <Navigation />
+        <BoxShadow />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/text-shadow",
+    element: (
+      <Core>
+        <Navigation />
+        <TextShadow />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/rgba",
+    element: (
+      <Core>
+        <Navigation />
+        <RGBA />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/font-face",
+    element: (
+      <Core>
+        <Navigation />
+        <FontFace />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/multiple-columns",
+    element: (
+      <Core>
+        <Navigation />
+        <MultipleColumns />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/box-resize",
+    element: (
+      <Core>
+        <Navigation />
+        <BoxResize />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/box-sizing",
+    element: (
+      <Core>
+        <Navigation />
+        <BoxSizing />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/outline",
+    element: (
+      <Core>
+        <Navigation />
+        <Outline />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/transition",
+    element: (
+      <Core>
+        <Navigation />
+        <Transition />
+      </Core>
+    ),
+  },
+  {
+    path: "/css-generator-react/transform",
+    element: (
+      <Core>
+        <Navigation />
+        <Transform />
+      </Core>
+    ),
+  },
+]);
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -120,6 +231,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={import.meta.env.MODE === "production" ? routerProd : router} />
   </React.StrictMode>
 );
